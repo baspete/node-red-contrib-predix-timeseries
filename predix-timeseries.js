bccb67000d665aac3b6fc5daa405ba221b18d244
Pete Butler <pete.butler@ge.com>
2017-11-09 13:41:06 -0800

    using predix-uaa-util

--- predix-timeseries.js
+++ predix-timeseries.js
@@ -19,10 +19,10 @@
  **/
 
 var request = require('request');
-var ws = require("ws");
+// var ws = require("ws");
 const uaa_util = require('predix-uaa-client');
-
-
+const WebSocketClient = require('websocket').client;
+const url = require('url');
 const SECONDS_CONVERT_TO_MS = 1000;
 const defaultQueryUrlPrefix = "https://time-series-store-predix.run.aws-usw02-pr.ice.predix.io/v1/";
 const defaultWsURL = "wss://gateway-predix-data-services.run.aws-usw02-pr.ice.predix.io/v1/stream/messages";
@@ -30,6 +30,9 @@ const originPath = "http://localhost/";
 
 module.exports = function(RED){
   "use strict";
+
+  // ******************************************************************
+  // Config Node
 
   function timeseriesClientNode(n){
 
@@ -46,41 +49,6 @@ module.exports = function(RED){
     //add the oauth endpoint to the UAA host url
     node.UAAurl += '/oauth/token';
 
-    // var buffer = new Buffer(node.clientID+":"+node.clientSecret);
-    // node.base64ClientCredential = buffer.toString('base64');
-
-    // var options ={
-    //   url: node.UAAurl,
-    //   headers:{
-    //     'Content-Type':'application/x-www-form-urlencoded',
-    //     'Pragma':'no-cache',
-    //     'Cache-Control':'no-cache',
-    //     'authorization':'Basic '+node.base64ClientCredential
-    //   },
-    //   method:'POST',
-    //   body:'username='+node.credentials.userID+'&password='+node.credentials.userSecret+'&grant_type=password'
-    // };
-
-    // request(options, function(error, response, body){
-    //   if(response && response.statusCode!==200){
-    //     node.error(response.statusCode+": "+response.statusMessage);
-    //     node.emit('unauthenticated','');
-    //   } else if(response){
-    //     try {
-    //       node.accessToken = JSON.parse(response.body).access_token;
-    //       node.refreshToken = JSON.parse(response.body).refresh_token;
-
-    //       node.emit('authenticated','');
-    //       node.tokenExpiryTime = (new Date).getTime() + JSON.parse(response.body).expires_in*SECONDS_CONVERT_TO_MS;
-    //     } catch (err) {
-    //       node.emit('accessTokenError');
-    //     }
-    //   } else {
-    //     node.error("Invalid request");
-    //     node.emit('unauthenticated','');
-    //   }
-    // });
-
     this.on('close', function(){
       /* nothing for now */
     });
@@ -93,151 +61,12 @@ module.exports = function(RED){
     }
   });
 
+  // Config Node
+  // ******************************************************************
 
-  function timeseriesIngestNode(config){
-    RED.nodes.createNode(this,config);
-    var node = this;
-    var isWsConnected = false;
-    this.server = RED.nodes.getNode(config.server);
 
-    if(this.server){
-      node.predixZoneId = node.server.predixZoneId;
-      node.accessToken = node.server.accessToken;
-
-      if(node.predixZoneId && node.accessToken){
-        startconn();
-      };
-
-      this.server.on('authenticated', function() {
-        node.log("[Predix Timeseries]: authenticated");
-        node.unauthorized = false;
-        node.status({fill:"green",shape:"dot",text:"Authenticated"});
-        node.predixZoneId = node.server.predixZoneId;
-        node.accessToken = node.server.accessToken;
-        startconn();
-      });
-
-      this.server.on('unauthenticated',function() {
-        node.log("[Predix Timeseries]: unauthenticated");
-        node.unauthorized = true;
-        node.status({fill:"red",shape:"ring",text:"Unauthenticated"});
-        node.predixZoneId = "";
-        node.accessToken = "";
-      });
-
-      this.server.on('accessTokenError',function() {
-        node.error("[Predix Timeseries]: access token error");
-        node.unauthorized = true;
-        node.status({fill:"red",shape:"ring",text:"Access Error"});
-        node.predixZoneId = "";
-        node.accessToken = "";
-      });
-    } else {
-      node.status({fill:"yellow", shape:"dot",text:"Missing config"});
-    }
-
-    //ws connection
-    function startconn(){
-      node.log("[Predix Timeseries]: start connection");
-      var opts = {};
-      if(node.predixZoneId && node.accessToken) {
-        opts={
-          headers:{
-            'predix-zone-id':node.predixZoneId,
-            'authorization':'Bearer '+node.accessToken,
-            'origin':originPath
-          }
-        };
-      } else {
-        if (!node.accessToken) {
-          node.status({fill:"red",shape:"ring",text:"missing access token"});
-        } else if (!node.predixZoneId) {
-          node.status({fill:"red",shape:"ring",text:"missing predix zone id"});
-        }
-        return;
-      }
-      var socket = new ws(wsURL, opts);
-      node.connection = socket;
-      handleConnection(node.connection);
-    }
-
-    function handleConnection(/*socket*/socket){
-      socket.on('open', function(){
-        node.log("[Predix Timeseries]: websocket is connected");
-        isWsConnected = true;
-        node.emit('opened','');
-        node.status({fill:"green",shape:"dot",text:"Connected"});
-      });
-
-      socket.on('close',function(code, data){
-        node.log("[Predix Timeseries]: websocket is closed");
-        isWsConnected = false;
-        node.status({fill:"red",shape:"ring",text:"Closed"});
-        node.emit('closed');
-
-        //reconnect
-        if(node.accessToken != ""){
-          clearTimeout(node.tout);
-          node.emit('reconnecting');
-          node.status({fill:"yellow",shape:"ring",text:"Reconnecting"});
-          node.tout = setTimeout(function(){ startconn(); }, 3000);
-        };
-      });
-
-      socket.on('error', function(err){
-        isWsConnected = false;
-        node.error(err);
-
-        node.status({fill:"red",shape:"ring",text:"Error"});
-
-        if(node.server.checkTokenExpire()){
-          node.server.renewToken(node.server);
-          if(node.accessToken != ""){
-            clearTimeout(node.tout);
-            node.emit('reconnecting');
-            node.status({fill:"yellow",shape:"ring",text:"Reconnecting"});
-            node.tout = setTimeout(function(){ startconn(); }, 3000);
-          }
-        }
-      });
-
-      socket.on('message',function(data){
-        node.log(data);
-        var statusCode;
-        try {
-          statusCode = JSON.parse(data).statusCode;
-        } catch (err) {
-          node.error("Invalid status code");
-        }
-        if(statusCode !== 202 ){
-          node.error(statusCode + ": " + "Ingest error");
-        };
-      })
-    }
-
-    this.on("input", function(msg){
-      var payload;
-      if (msg.hasOwnProperty("payload")) {
-        if (!Buffer.isBuffer(msg.payload)) { // if it's not a buffer make sure it's a string.
-          payload = RED.util.ensureString(msg.payload);
-        } else {
-          payload = msg.payload;
-        }
-      }
-      if(isWsConnected === true){
-        if (payload) {
-            try {
-              node.connection.send(payload);
-            } catch(err){
-              node.error(err);
-            }
-        }
-      } else {
-        node.error("[Predix Timeseries]: Websocket not connected");
-      }
-    });
-  }
-  RED.nodes.registerType("timeseries-ingest", timeseriesIngestNode);
+  // ******************************************************************
+  // Query Node
 
   function timeseriesQueryNode(config){
     RED.nodes.createNode(this,config);
@@ -247,30 +76,29 @@ module.exports = function(RED){
 
     this.server = RED.nodes.getNode(config.server);
 
+    // Indicator
     if(this.server){
-      node.predixZoneId = node.server.predixZoneId;
-      this.server.on('authenticated', function() {
+      node.on('authenticated', function() {
         node.unauthorized = false;
         node.status({fill:"green",shape:"dot",text:"Authenticated"});
-        node.predixZoneId = node.server.predixZoneId;
-        node.accessToken = node.server.accessToken;
       });
-      this.server.on('unauthenticated',function() {
+      node.on('unauthenticated',function() {
         node.unauthorized = true;
         node.status({fill:"red",shape:"ring",text:"Unauthenticated"});
-        node.predixZoneId = "";
-        node.accessToken = "";
       });
-      this.server.on('accessTokenError',function() {
+      node.on('requestError',function() {
         node.unauthorized = true;
-        node.status({fill:"red",shape:"ring",text:"Access Error"});
-        node.predixZoneId = "";
-        node.accessToken = "";
+        node.status({fill:"red",shape:"ring",text:"Request Error"});
+      });
+      node.on('responseError',function() {
+        node.unauthorized = true;
+        node.status({fill:"red",shape:"ring",text:"Response Error"});
       });
     } else {
       node.status({fill:"yellow", shape:"dot",text:"Missing server config"});
     }
 
+    // Method/Routes for query Types
     switch(node.queryType){
       case "aggregations":
         node.apiEndpoint = node.server.queryUrlPrefix + "aggregations";
@@ -301,17 +129,14 @@ module.exports = function(RED){
           node.error("Failed to parse msg.payload: " + err);
           return;
         }
-        // Call with client credentials (UAAUrl, ClientID, ClientSecret),
-        // will fetch a client token using these credentials.
-        // In this case the client needs authorized_grant_types: client_credentials
+        // Call with client credentials (UAAUrl, ClientID, ClientSecret), will fetch a client token using these credentials.
+        // uaa_util will cache token for these credentials and retrieve a refresh token if needed.
         uaa_util.getToken(node.server.UAAurl, node.server.clientID, node.server.clientSecret).then((token) => {
-            // Use token.access_token as a Bearer token Authroization header
-            // in calls to secured services.
-
+          node.emit('authenticated','');
           request({
               url: node.apiEndpoint,
               headers:{
-                'predix-zone-id':node.predixZoneId,
+                'predix-zone-id':node.server.predixZoneId,
                 'authorization':'Bearer '+token.access_token
               },
               method:requestMethod,
@@ -319,16 +144,18 @@ module.exports = function(RED){
             }, function(error, response, body){
             if(error){
               node.error(error);
+              node.emit('requestError','');
             } else if(response) {
               if (response.statusCode!==200){
+                node.emit('responseError','');
                 node.error(response.statusCode+": "+response.body);
               } else {
                 node.send({payload:response.body});
               }
             }
           });
-
         }).catch((err) => {
+          node.emit('unauthenticated','');
           node.error(err);
         });
       }
@@ -339,5 +166,123 @@ module.exports = function(RED){
     });
   }
   RED.nodes.registerType("timeseries-query", timeseriesQueryNode);
-}
 
+  // Query Node
+  // ******************************************************************
+
+  // ******************************************************************
+  // Ingest Node
+
+  function timeseriesIngestNode(config){
+    RED.nodes.createNode(this,config);
+    var node = this;
+    var isWsConnected = false;
+    const client = new WebSocketClient();
+
+    node.server = RED.nodes.getNode(config.server);
+
+    // Indicator for Auth Events
+    if(node.server){
+      node.server.on('authenticated', function() {
+        node.log("[Predix Timeseries]: authenticated");
+        node.status({fill:"green",shape:"dot",text:"Authenticated"});
+      });
+      node.server.on('unauthenticated',function() {
+        node.log("[Predix Timeseries]: unauthenticated");
+        node.status({fill:"red",shape:"ring",text:"Unauthenticated"});
+      });
+      node.server.on('accessTokenError',function() {
+        node.error("[Predix Timeseries]: access token error");
+        node.status({fill:"red",shape:"ring",text:"Access Error"});
+      });
+    } else {
+      node.status({fill:"yellow", shape:"dot",text:"Missing config"});
+    }
+
+
+    // Get a token
+    uaa_util.getToken(node.server.UAAurl, node.server.clientID, node.server.clientSecret).then((token) => {
+      node.emit('authenticated','');
+
+      // Proxy check for websocket
+      let requestOptions = {};
+      if(process.env.https_proxy) {
+        const proxy = url.parse(process.env.https_proxy);
+        const tunnelingAgent = require('tunnel').httpsOverHttp({
+          proxy: {
+            host: proxy.hostname,
+            port: proxy.port
+          }
+        });
+        node.log("[Predix Timeseries]: Using Proxy "+ proxy.host);
+        requestOptions.agent = tunnelingAgent;
+      }
+
+      const headers = {
+        'predix-zone-id':node.server.predixZoneId,
+        'authorization':'Bearer '+token.access_token,
+        'Origin':originPath
+      };
+
+      client.on('connect', connection => {
+
+        node.log("[Predix Timeseries]: Websocket Connected");
+        node.status({fill:"green",shape:"dot",text:"Connected"});
+
+        connection.on('error', error => {
+          node.error("[Predix Timeseries]: Websocket Connected");
+          node.status({fill:"red",shape:"ring",text:"Websocket Error"});
+        });
+
+
+        connection.on('message', data => {
+          node.log(data);
+          console.log('message received', data);
+          var statusCode;
+          try {
+            statusCode = JSON.parse(data).statusCode;
+          } catch (err) {
+            node.error("Invalid status code", statusCode);
+          }
+          if(statusCode !== 202 ){
+            node.error(statusCode + ": " + "Ingest error");
+          };
+        });
+
+        // When data is received on the input to this node
+        node.on("input", function(msg){
+          var payload;
+          if (msg.hasOwnProperty("payload")) {
+            if (!Buffer.isBuffer(msg.payload)) { // if it's not a buffer make sure it's a string.
+              payload = RED.util.ensureString(msg.payload);
+            } else {
+              payload = msg.payload;
+            }
+          }
+          if (payload) {
+            try {
+              node.log('sending payload', payload);
+              connection.send(payload);
+            } catch(err){
+              node.error(err);
+            }
+          }
+        });
+
+      });
+
+      // Open the web socket
+      client.connect(node.server.wsUrl, null, originPath, headers, requestOptions);
+
+    }).catch((err) => {
+      node.emit('unauthenticated','');
+      node.error(err);
+    });
+
+  }
+  RED.nodes.registerType("timeseries-ingest", timeseriesIngestNode);
+
+  // Ingest
+  // ******************************************************************
+
+}
